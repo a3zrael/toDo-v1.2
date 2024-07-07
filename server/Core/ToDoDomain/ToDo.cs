61c6bc76edca90411a8f1e172795dac1807f180c
Алексей Шелухин <sheluhinalekseypavlovich@gmail.com>
2024-07-07 14:16:14 +0300

    serv/init: Инициализировал бд, создал простейшее REST Web API

---
+++ server/Core/ToDoDomain/ToDo.cs
@@ -0,0 +1,6 @@
+﻿public class ToDo {
+    public string Id { get; set; } = "";
+    public string Title { get; set; } = "";
+    public string Details { get; set; } = "";
+    public bool Completed { get; set; }
+}
--- server/PL/ToDoWebAPI/Program.cs
+++ server/PL/ToDoWebAPI/Program.cs
@@ -1,6 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var _connection = builder.Configuration.GetConnectionString("connection");
+builder.Services.AddDbContext<ToDoDbContext>(options => options.UseNpgsql(_connection));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/todolist", async (ToDoDbContext db) => await db.toDos.ToListAsync());
+
+app.MapGet("/api/todolist/{id}", async (string id, ToDoDbContext db) => {
+    ToDo? td = await db.toDos.FirstOrDefaultAsync(td => td.Id == id);
+    if (td == null) return Results.NotFound(new {message = "Пользователь не найден"});
+    return Results.Json(td);
+});
+
+app.MapPost("/api/todolist", async (ToDo td, ToDoDbContext db) =>{
+    await db.toDos.AddAsync(td);
+    await db.SaveChangesAsync();
+    return td;
+});
+
+app.MapDelete("/api/todolist/{id}", async (string id, ToDoDbContext db) => {
+    ToDo? td = await db.toDos.FirstOrDefaultAsync(td => td.Id == id);
+    if (td == null) return Results.NotFound(new {message = "Пользователь не найден"});
+    db.toDos.Remove(td);
+    await db.SaveChangesAsync();
+    return Results.Json(td);
+});
+
+app.MapPut("/api/todolist/{id}", async (ToDo _td, ToDoDbContext db) => {
+    var td = await db.toDos.FirstOrDefaultAsync(td => td.Id == _td.Id); 
+    if(td == null) return Results.NotFound(new { message = "Пользователь не найден"});
+
+    td.Title = _td.Title;
+    td.Details = _td.Details;
+    td.Completed = _td.Completed;
+    await db.SaveChangesAsync();
+    return Results.Json(td);
+});
 
 app.Run();
